ca9ac0d227c6f7d3452a792577802ad8fd9c8c5f
Bhakti Kundhare <273702315+pratiksha-2026@users.noreply.github.com>
2026-05-19 11:15:42 +0530

    added Render database logic

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,18 +1,29 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt           
-from flask_login import LoginManager      
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
-# You MUST have a SECRET_KEY for login forms to work securely!
-# If you don't have this line, add it now:
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245' 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-# This initializes SQLAlchemy
+# --- THE SMART DATABASE LOGIC ---
+# This line looks to see if Render gave us an internet database
+database_url = os.environ.get('DATABASE_URL')
+
+if database_url:
+    # If Render DID give us a database, we fix the name and use it
+    if database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+else:
+    # If we are on your PC, it uses your local file
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# --------------------------------------
+
 db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)                      # <-- NEW
-login_manager = LoginManager(app)         # <-- NEW
-login_manager.login_view = 'login'        # <-- NEW (Tells Flask where the login page is)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 from flaskblog import routes
